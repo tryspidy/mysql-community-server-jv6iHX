bd3fae65c2648cfba444e76504c261793f582fdb
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-08 11:53:52 +0530

    Initial commit

---
+++ main.sql
@@ -0,0 +1,11 @@
+/* Answer to: "mysql community server" */
+
+/*
+  MySQL is an open-source relational database management system. Its
+  name is a combination of "My", the name of co-founder Michael
+  Widenius's daughter, and "SQL", the abbreviation for Structured
+  Query Language.
+  
+  To download the community server application go to:
+  https://dev.mysql.com/downloads/mysql/
+*/
